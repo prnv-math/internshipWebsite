e8e4de7f41d4705e74e01bd2e4836fbd812a0f9c
prnv-math <pranavmonline@gmail.com>
2026-06-01 10:49:21 +0530

    new pages update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
-// document.documentElement.style.setProperty('--primaryColor', '#101010');
-
+// Navbar script
 window.addEventListener('scroll', () => {
     document.getElementById('mainNavbar')
       .classList.toggle('scrolled', window.scrollY > 50);
